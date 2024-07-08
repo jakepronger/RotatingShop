cf4f4464aa6d3a984b7b35087847b0ee6060d6b1
Jake Pronger <jakepronger31@gmail.com>
2024-07-08 19:10:22 +1000

    update

---
+++ src/main/java/me/jakepronger/rotatingshop/utils/ConfigUtils.java
@@ -0,0 +1,53 @@
+package me.jakepronger.rotatingshop.utils;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+import static me.jakepronger.rotatingshop.RotatingShop.plugin;
+
+public class ConfigUtils {
+
+    public final String filePath;
+    public final File file;
+
+    private final FileConfiguration config;
+
+    public ConfigUtils(String fileName) {
+
+        filePath = plugin.getDataFolder() + File.separator + fileName;
+
+        file = new File(filePath);
+
+        if (!file.exists()) {
+            try {
+                if (!file.createNewFile())
+                    throw new Exception("createNewFile() returned false for \"" + file.getName() + "\"");
+            } catch (Exception e) {
+                Logger.error(e.getMessage());
+            }
+        }
+
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void save(FileConfiguration config) {
+
+        if (file == null) {
+            Logger.error("Failed to save unknown file because it's null.");
+            return;
+        }
+
+        try {
+            config.save(file);
+        } catch (Exception e) {
+            Logger.error(e.getMessage());
+        }
+    }
+
+}
