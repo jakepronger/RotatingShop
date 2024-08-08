4ad594087794135a4a871f43f8b3990db5f743ae
Jake Pronger <jakepronger31@gmail.com>
2024-08-08 22:14:13 +1000

    RotationUtils

--- src/main/java/me/jakepronger/rotatingshop/utils/RotationUtils.java
+++ src/main/java/me/jakepronger/rotatingshop/utils/RotationUtils.java
@@ -1,8 +1,22 @@
 package me.jakepronger.rotatingshop.utils;
 
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+
 public class RotationUtils {
 
-    //
+    private int itemsAmount;
+    private HashMap<Integer, ItemStack> items;
+
+    public RotationUtils() {
+        //plugin.getcon
+    }
+
+    private void initiateVars() {
+
+    }
 
     public void reload() {
 
